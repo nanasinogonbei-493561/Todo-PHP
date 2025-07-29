b3b91e51dff294b72b9cde51aef7de2e56212e67
菅原　祥宏 <sugawara@sugawarasachihiroshinoMacBook-Air.local>
2025-07-30 00:09:38 +0900

    マイグレーションファイルの変更をしました。

--- database/migrations/2025_06_05_141258_create_todos_table.php
+++ database/migrations/2025_06_05_141258_create_todos_table.php
@@ -9,13 +9,13 @@ return new class extends Migration
 */
 public function up(): void
 {
-Schema::create('todos', function (Blueprint $table) {
-$table->id();
-$table->string('title');
-$table->text('description')->nullable();
-$table->foreignId('user_id')->constrained()->onDelete('cascade');
-$table->timestamps();
-});
+    Schema::create('todos', function (Blueprint $table) {
+        $table->id();
+        $table->string('title');
+        $table->text('description')->nullable();
+        $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        $table->timestamps();
+    });
 }
 /**
  * Reverse the migrations.
